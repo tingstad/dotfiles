48aefdb57fc6fdff9d62c4d8ce89c50afc439a9a
Richard Tingstad <richard.tingstad@gmail.com>
2016-10-10 21:24:18 +0200

    Only back up non-link files

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@
 find -maxdepth 1 \( -name '.*' -or -name gitignore \) -type f -not -name '.*.swp' -not -name '.gitignore' -print \
     | while read f; do
         fil=~/$(basename $f)
-        [ -e $fil ] && cp -L $fil $fil.old
+        [ -e $fil -a ! -L $fil ] && cp -L $fil $fil.old
         ln -sf -t ~ $(readlink -f $f)
     done
 
