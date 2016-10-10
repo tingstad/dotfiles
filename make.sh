d6275437a0ff85d304df02a1aa27a9299f27db09
Richard Tingstad <richard.tingstad@gmail.com>
2016-10-10 21:49:06 +0200

    Better handle file names with spaces

--- make.sh
+++ make.sh
@@ -4,8 +4,8 @@
 
 find -maxdepth 1 \( -name '.*' -or -name gitignore \) -type f -not -name '.*.swp' -not -name '.gitignore' -print \
     | while read f; do
-        fil=~/$(basename $f)
-        [ -e $fil -a ! -L $fil ] && cp -L $fil $fil.old
-        ln --symbolic --force --target-directory="$HOME" "$(readlink --canonicalize $f)"
+        fil="$HOME/$(basename "$f")"
+        [ -e "$fil" -a ! -L "$fil" ] && cp -L "$fil" "$fil.old"
+        ln --symbolic --force --target-directory="$HOME" "$(readlink --canonicalize "$f")"
     done
 
