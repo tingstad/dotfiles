cd8e6623e3072494e0cb79f99e4e819a4af21b71
Richard Tingstad <richard.tingstad@gmail.com>
2019-02-08 22:48:25 +0100

    Refactor

--- make.sh
+++ make.sh
@@ -3,21 +3,22 @@ set -o errexit
 
 # inspired by https://github.com/jfrazelle/dotfiles
 
-ABSPATH=$(cd "$(dirname "$0")"; pwd)
-
 main() {
-    link_dotfiles
-    add_aliases "$ABSPATH/aliases.sh" "$HOME/.bashrc"
+    local src_dir=$(cd "$(dirname "$0")"; pwd)
+    link_dotfiles "$src_dir" "$HOME"
+    add_aliases "$src_dir/aliases.sh" "$HOME/.bashrc"
 }
 
 link_dotfiles() {
-    find "$ABSPATH" -maxdepth 1 \
+    local src_dir="$1"
+    local target_dir="$2"
+    find "$src_dir" -maxdepth 1 \
     \( -name '.*' -or -name gitignore \) \
     -type f \
     -not -name '.*.swp' -not -name '.gitignore' -not -name .bashrc \
     -print \
     | while read f; do
-        fil="$HOME/$(basename "$f")"
+        local fil="$target_dir/$(basename "$f")"
         [ -e "$fil" -a ! -L "$fil" ] && cp -L "$fil" "$fil.old"
         ln -s -f "$f" "$fil"
     done
@@ -25,17 +26,19 @@ link_dotfiles() {
 
 add_aliases() {
     local alias_file="$1"
-    local dest="$2"
+    local target="$2"
     local head="#BEGIN TINGSTAD DOTFILES"
     local tail="#END TINGSTAD DOTFILES"
-    if [ -f "$dest" ] && grep -q "$head" "$dest"; then
-        { rm "$dest" && \
-        awk '/'"$head"'/{ skip=1;print } /'"$tail"'/{ skip=0; print "source \"'"$alias_file"'\""  } !skip{ print }' \
-        > "$dest"; } < "$dest"
+    if [ -f "$target" ] && grep -q "$head" "$target"; then
+        { rm "$target" && \
+        awk '/'"$head"'/{ skip=1; print }
+            /'"$tail"'/{ skip=0; print "source \"'"$alias_file"'\"" }
+            !skip{ print }' \
+        > "$target"; } < "$target"
     else
-        echo "$head" >> "$dest"
-        echo "source \"$alias_file\"" >> "$dest"
-        echo "$tail" >> "$dest"
+        echo "$head" >> "$target"
+        echo "source \"$alias_file\"" >> "$target"
+        echo "$tail" >> "$target"
     fi
 }
 
@@ -43,5 +46,5 @@ if [ "$TESTMODE" = "on" ]; then
     return
 fi
 
-main
+main "$@"
 
