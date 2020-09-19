4c88019f201754f38da7e0ee24f5bb1e2921562a
Richard Tingstad <richard.tingstad@gmail.com>
2020-09-19 10:05:02 +0200

    Do not print "SIGINT", just quit

--- gitlog.sh
+++ gitlog.sh
@@ -4,7 +4,7 @@ set -e
 
 main() {
     local file="${1:-.}"
-    trap 'echo SIGINT; quit' SIGINT
+    trap 'quit' SIGINT
     trap 'redraw' SIGWINCH
     command -v tmux >/dev/null || {
         echo "tmux not found :(" >&2
