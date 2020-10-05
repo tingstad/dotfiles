361eff116403659bf9f9061b56df2b18aa8a741a
Richard Tingstad <richard.tingstad@gmail.com>
2020-10-05 20:01:36 +0200

    More portable

--- gitlog.sh
+++ gitlog.sh
@@ -165,6 +165,17 @@ ccut() {
     }'
 }
 
-return 2>/dev/null || true
+sourced=0
+if [ -n "$ZSH_EVAL_CONTEXT" ]; then
+    case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
+elif [ -n "$KSH_VERSION" ]; then
+    [ "$(cd $(dirname -- $0) && pwd -P)/$(basename -- $0)" != "$(cd $(dirname -- ${.sh.file}) && pwd -P)/$(basename -- ${.sh.file})" ] && sourced=1
+elif [ -n "$BASH_VERSION" ]; then
+    (return 0 2>/dev/null) && sourced=1
+#else # other shells: examine for known shell binary filenames. Detects `sh` and `dash`; add additional shell filenames as needed.
+#    case ${0##*/} in sh|dash) sourced=1;; esac
+fi
 
-main "$@"
+if [ $sourced -eq 0 ]; then
+    main "$@"
+fi
