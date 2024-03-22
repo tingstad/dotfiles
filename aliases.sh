2cdf01fa142bdf086de88802bee2460a8b8dfe19
Richard Tingstad <richard.tingstad@gmail.com>
2024-03-22 22:31:23 +0100

    add bin/,grip

--- aliases.sh
+++ aliases.sh
@@ -8,17 +8,7 @@ alias ,touchbar='sudo pkill TouchBarServer'
 alias ,touchbar2='sudo killall “ControlStrip”'
 
 grip(){
-    case "/$3/ /$(pwd)/" in
-        */node_modules/*)
-            filter_node="" ;;
-        */target/*)
-            filter_target="" ;;
-        *)
-            filter_node="-or -type d -and -name node_modules"
-            filter_target="-or -type d -and -name target"
-        ;;
-    esac
-    find "${3-.}" \( -name '.[^.]*' $filter_node $filter_target \) -prune -or -name "${2:-*}" -type f -print0 | xargs -0 egrep --binary-files=without-match ${@:4} "$1"
+    ,grip "$@"
 }
 
 calc() { awk "BEGIN{ print $* }" ;}
