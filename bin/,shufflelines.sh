22563d35290ed56cec57ccedb80f7be55f50cf77
Richard Tingstad <richard.tingstad@gmail.com>
2026-02-23 10:48:28 +0100

    ,shufflelines.sh impl

--- bin/,shufflelines.sh
+++ bin/,shufflelines.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 set -e
 
-tmpdir=${TMPDIR:-/tmp}
-tmpdir=${tmpdir%/}
+# https://stackoverflow.com/a/30133294 by mklement0, License - CC BY-SA 4.0
+awk 'BEGIN {srand(); OFMT="%.17f"} {print rand(), $0}' "$@" \
+    | sort -k1,1n \
+    | cut -d ' ' -f2-
 
-test -f "$1"
-
-n=$(wc -l "$1" | awk '{ print $1 }')
-
-tmp=${tmpdir}/${1}$$
-
-i=$n
-while [ $i -gt 0 ]; do
-    awk 'BEGIN     { srand('$RANDOM'); v = int(rand() * '$i') + 1 }
-        NR == v    { s = $0; next }
-                   { print $0 }
-        END        { print s }' \
-        <"$1" >"$tmp" && mv "$tmp" "$1"
-    i=$(( i - 1 ))
-done
-
+#tmpdir=${TMPDIR:-/tmp}
+#tmpdir=${tmpdir%/}
+#
+#test -f "$1"
+#
+#n=$(wc -l "$1" | awk '{ print $1 }')
+#
+#tmp=${tmpdir}/${1}$$
+#
+#i=$n
+#while [ $i -gt 0 ]; do
+#    awk 'BEGIN     { srand('$RANDOM'); v = int(rand() * '$i') + 1 }
+#        NR == v    { s = $0; next }
+#                   { print $0 }
+#        END        { print s }' \
+#        <"$1" >"$tmp" && mv "$tmp" "$1"
+#    i=$(( i - 1 ))
+#done
